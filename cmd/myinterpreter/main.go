6bd34f8b3103f6cc6f0eda74b23ccb149ad1ce06
Feras Muki <feras.muki@gmail.com>
2024-07-22 14:15:20 -0400

    Move tokenization to a separate scanner file

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -6,24 +6,6 @@ import (
 	"strings"
 )
 
-func tokenize(lox_file_contents string) []string {
-	var tokenization []string
-	for i := 0; i < len(lox_file_contents); i++ {
-		char := lox_file_contents[i]
-		if char == '('{
-			tokenization = append(tokenization, "LEFT_PAREN ( null")
-		}else if char == ')' {
-			tokenization = append(tokenization, "RIGHT_PAREN ) null")
-		}else if char == '{' {
-			tokenization = append(tokenization, "LEFT_BRACE { null")
-		}else if char == '}' {
-			tokenization = append(tokenization, "RIGHT_BRACE } null")
-		}
-	}
-	tokenization = append(tokenization, "EOF  null")
-	return tokenization
-}
-
 func main() {
 
 	if len(os.Args) < 3 {
