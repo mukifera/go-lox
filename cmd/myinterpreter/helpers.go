a3dc0b8b607f9bbb27e7b2ca35102e6bff2a4f3e
Feras Muki <feras.muki@gmail.com>
2024-07-25 13:59:55 -0400

    extrace generic helper functions to a separate file

---
+++ cmd/myinterpreter/helpers.go
@@ -0,0 +1,40 @@
+package main
+
+import "math/big"
+
+func isAlpha(c byte) bool {
+	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'z') || (c == '_')
+}
+
+func isDigit(c byte) bool {
+	return '0' <= c && c <= '9'
+}
+
+func isAlphaNumeric(c byte) bool {
+	return isAlpha(c) || isDigit(c)
+}
+
+func stringToBigFloat(str string) big.Float {
+	var power, digit, ten, tmp, float_literal big.Float
+	power.SetFloat64(1.0)
+	ten.SetFloat64(10.0)
+	float_literal.SetFloat64(0)
+	found_period := false
+	for _, char := range str {
+		if char == '.' {
+			found_period = true
+			continue
+		}
+		if found_period {
+			digit.SetFloat64(float64(char - '0'))
+			power.Quo(&power, &ten)
+			tmp.Mul(&power, &digit)
+			float_literal.Add(&float_literal, &tmp)
+		} else {
+			digit.SetFloat64(float64(char - '0'))
+			tmp.Mul(&float_literal, &ten)
+			float_literal.Add(&tmp, &digit)
+		}
+	}
+	return float_literal
+}
--- cmd/myinterpreter/scanner.go
+++ cmd/myinterpreter/scanner.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"math/big"
 	"os"
 )
 
@@ -82,43 +81,6 @@ func (scanner *Scanner) PeekNext() byte {
 		return 0
 	}
 	return scanner.contents[scanner.current + 1]
-}
-
-func isAlpha(c byte) bool {
-	return ('a' <= c && c <= 'z') || ('A' <= c && c <= 'z') || (c == '_')
-}
-
-func isDigit(c byte) bool {
-	return '0' <= c && c <= '9'
-}
-
-func isAlphaNumeric(c byte) bool {
-	return isAlpha(c) || isDigit(c)
-}
-
-func stringToBigFloat(str string) big.Float {
-	var power, digit, ten, tmp, float_literal big.Float
-	power.SetFloat64(1.0)
-	ten.SetFloat64(10.0)
-	float_literal.SetFloat64(0)
-	found_period := false
-	for _, char := range str {
-		if char == '.' {
-			found_period = true
-			continue
-		}
-		if found_period {
-			digit.SetFloat64(float64(char - '0'))
-			power.Quo(&power, &ten)
-			tmp.Mul(&power, &digit)
-			float_literal.Add(&float_literal, &tmp)
-		} else {
-			digit.SetFloat64(float64(char - '0'))
-			tmp.Mul(&float_literal, &ten)
-			float_literal.Add(&tmp, &digit)
-		}
-	}
-	return float_literal
 }
 
 func (scanner *Scanner) ScanNumber() {
