bf8f8191d044e9fe1b5dcd7cfd66559729ce4f57
Feras Muki <feras.muki@gmail.com>
2024-07-25 16:19:10 -0400

    Implement string literal parsing

--- cmd/myinterpreter/parse.go
+++ cmd/myinterpreter/parse.go
@@ -8,7 +8,7 @@ func Parse(tokens []Token) {
 		case EOF: break;
 		case TRUE: fmt.Println("true"); break;
 		case FALSE: fmt.Println("false"); break;
-		case NUMBER: fmt.Println(token.StringLiteral()); break;
+		case NUMBER, STRING: fmt.Println(token.StringLiteral()); break;
 		default: fmt.Println("nil"); break;
 		}
 	}
