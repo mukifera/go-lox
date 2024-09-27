5c25a55f4a24ddabe6c4064037ee3a6332ce41db
Feras Muki <feras.muki@gmail.com>
2024-09-27 11:57:00 -0400

    move the runtime tests to a yaml file

--- cmd/myinterpreter/run_test.go
+++ cmd/myinterpreter/run_test.go
@@ -1,92 +1,42 @@
 package main
 
 import (
+	"os"
+
 	"bytes"
 	"testing"
+
+	"gopkg.in/yaml.v2"
 )
 
+type test_config struct {
+	Name         string `yaml:"name"`
+	FileContents string `yaml:"fileContents"`
+	Expected     string `yaml:"expected"`
+}
+
 func TestRuntime(t *testing.T) {
-	tests := []struct {
-		name string
-		fileContents string
-		expected string
-	}{
-		{"Print #1", `print "Hello, World!";`, "Hello, World!\n"},
-		{"Print #2", "print 42;", "42\n"},
-		{"Print #3", "print true;", "true\n"},
-		{"Print #4", "print 12 + 24;", "36\n"},
 
-		{
-			"Multiple Statements #1",
+	var tests []test_config
 
-`print "world" + "baz" + "bar";
-print 27 - 26;
-print "bar" == "quz";`,
-
-`worldbazbar
-1
-false
-`,
-		},
-
-		{
-			"Multiple Statements #2",
-
-`print "hello"; print true;
-print false;
-print "bar"; print 43;`,
-
-`hello
-true
-false
-bar
-43
-`,
-		},
-
-		{
-			"Multiple Statements #3",
-		
-`print 81;
-    print 81 + 46;
-        print 81 + 46 + 19;`,
-		
-`81
-127
-146
-`,
-		},
-		{
-			"Multiple Statements #4",
-		
-`print true != true;
-
-print "36
-10
-78
-";
-
-print "There should be an empty line above this.";`,
-		
-`false
-36
-10
-78
-
-There should be an empty line above this.
-`,
-		},
+	yamlFile, err := os.ReadFile("run_tests.yaml")
+	if err != nil {
+		t.Errorf("yamlFile.Get err   #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &tests)
+	if err != nil {
+		t.Errorf("Unmarshal: %v", err)
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			exprs := getExpressions(tt.fileContents, t)
+		t.Run(tt.Name, func(t *testing.T) {
+			exprs := getExpressions(tt.FileContents, t)
 			var buf bytes.Buffer
 			FRunExpressions(&buf, exprs)
 			str := buf.String()
 
-			if tt.expected != str {
-				t.Errorf("Execution result mismatch\nExpected:\n\n%s\nGot:\n\n%s", tt.expected, str)
+			if tt.Expected != str {
+				t.Errorf("Execution result mismatch\nExpected:\n\n%s\nGot:\n\n%s", tt.Expected, str)
 			}
 		})
 	}
--- go.mod
+++ go.mod
@@ -9,3 +9,5 @@
 module github.com/codecrafters-io/interpreter-starter-go
 
 go 1.22
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
